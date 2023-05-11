d4bc3a84ea991de5add0c6bc75a5d2af82a6a1ae
Steffen Geving <gevis1@student.op.ac.nz>
2023-05-11 16:47:09 +1200

    build: configure formatting and linting using prettier, eslint and husky now uses prettier for formatting, eslint with airbnb rules for linting, and husky configured on prepare stage to automatically format and lint on commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,26 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  env: {
+    browser: true,
+    es2021: true,
   },
-}
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  ignorePatterns: ['src/**/*.test.ts', '.eslintrc.cjs'],
+};
