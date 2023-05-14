3848bcc6b4cc81df8084937a99d446be5e3994b2
Steffen Geving <gevis1@student.op.ac.nz>
2023-05-15 11:57:54 +1200

    feat(App.tsx): add hello world to prove tailwindcss is working

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <div>Hello World</div>
-      <div>Hello World 2</div>
-    </>
+    <h1 className="text-3xl font-bold underline text-blue-700">Hello world!</h1>
   );
 }
 
