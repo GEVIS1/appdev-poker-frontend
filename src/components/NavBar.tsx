a308dbce9b867243b1e3a8c5051914f82dec8f18
Steffen Geving <gevis1@student.op.ac.nz>
2023-05-16 11:54:18 +1200

    feat(NavBar.tsx): add progress on hamburger menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import { AuthContext } from '../contexts/AuthContext';
 import Spinner from './Spinner';
@@ -7,6 +7,7 @@ function NavBar() {
   const {
     logOutUser, user, waitingForAuth, processingLogin, anonymousSignIn,
   } = useContext(AuthContext);
+  const [showHamburgerMenu, setShowHamburgerMenu] = useState(false);
 
   const handleLoginButton = async () => {
     if (anonymousSignIn) {
@@ -23,6 +24,21 @@ function NavBar() {
       logOutUser();
     }
   };
+
+  const testButtons = (
+    <>
+      {['Home', 'About', 'Contact', 'Address', 'Policy'].map((buttonName) => (
+        <button
+          className="bg-gray-400 text-white border-2 rounded-lg m-1"
+          key={buttonName}
+          type="button"
+          onClick={() => window.console.log(`${buttonName} clicked!`)}
+        >
+          {buttonName}
+        </button>
+      ))}
+    </>
+  );
 
   const loginButton = (
     <button
@@ -45,12 +61,35 @@ function NavBar() {
     </button>
   );
 
+  const menu = (
+    <div id="menu-container">
+      <nav id="menu-spread" className="hidden sm:block">
+        {testButtons}
+      </nav>
+      <div id="menu-hamburger" className="block sm:hidden">
+        <button
+          type="button"
+          className=""
+          onClick={() => setShowHamburgerMenu(true)}
+          onPointerLeave={() => setShowHamburgerMenu(false)}
+        >
+          Hamburger Icon
+        </button>
+        {showHamburgerMenu && (
+          <div className="absolute left-0 top-0 rounded-xl bg-slate-600 w-52 m-2 p-2">
+            <div className="flex flex-col">{...[loginButton, testButtons]}</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+
   return (
     <div
       id="navbar"
       className="h-10 flex items-center p-7 bg-green-600 rounded-b"
     >
-      {loginButton}
+      {menu}
     </div>
   );
 }
