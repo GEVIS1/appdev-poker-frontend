71bf9f98974d457cffe1c811804e7eb20cdbc95b
Steffen Geving <gevis1@student.op.ac.nz>
2023-05-15 17:08:15 +1200

    feat(firebase.ts): add export of firebaseAuth

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,4 +10,7 @@ import firebaseConfig from '../../firebaseConfig.json';
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+const firebaseAuth = getAuth(app);
+
 export default app;
+export { firebaseAuth };
