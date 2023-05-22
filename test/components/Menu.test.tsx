3ce12bc4a9f086ab6ac8db390bb344147e9dbf84
Steffen Geving <gevis1@student.op.ac.nz>
2023-05-23 11:46:16 +1200

    test: add in progress test for spinners being undefined

--- test/components/Menu.test.tsx
+++ test/components/Menu.test.tsx
@@ -1,5 +1,5 @@
 import {
-  describe, expect, it, vi,
+  describe, expect, it, vi, afterEach,
 } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
@@ -8,6 +8,23 @@ import Menu from '../../src/components/Menu';
 // Resize window
 // window.innerWidth = 200;
 // window.dispatchEvent(new Event('resize'));
+
+// waitingForAuth
+// processingLogin
+afterEach(() => {
+  vi.restoreAllMocks();
+});
+
+vi.mock('react', async () => {
+  const actual = await vi.importActual<object>('react');
+
+  return {
+    ...actual,
+    useContext: () => ({
+      logOutUser: () => {},
+    }),
+  };
+});
 
 describe('Menu component tests', () => {
   it('Should show spinner on login buttons while waiting for auth context', () => {
@@ -20,5 +37,17 @@ describe('Menu component tests', () => {
 
     expect(spinnerAnonymous).toBeVisible();
     expect(spinnerGoogle).toBeVisible();
+  });
+
+  it('Should not show spinner on login buttons while waiting for auth context', () => {
+    render(<Menu />);
+
+    const spinnerAnonymous = screen.getAllByTestId(
+      'login-spinner-anonymous',
+    )[0];
+    const spinnerGoogle = screen.getAllByTestId('login-spinner-google')[0];
+
+    expect(spinnerAnonymous).not.toBeVisible();
+    expect(spinnerGoogle).not.toBeVisible();
   });
 });
