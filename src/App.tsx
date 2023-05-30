775d5bf9f753814217d05e8426e141cb78711757
Steffen Geving <gevis1@student.op.ac.nz>
2023-05-30 15:20:09 +1200

    feat: add use of game component to app

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,16 @@ import { useContext } from 'react';
 import { AuthContext } from './contexts/AuthContext';
 import NavBar from './components/NavBar';
 import Spinner from './components/Spinner';
+import Game from './components/Game';
 
 function App() {
-  const { user, waitingForAuth, processingLogin } = useContext(AuthContext);
-
-  const content = (
-    <div className="m-4 flex justify-center items-center h-screen">
-      {!user ? <div>Not logged in...</div> : <div>Please add Game here</div>}
-    </div>
-  );
+  const { waitingForAuth, processingLogin } = useContext(AuthContext);
 
   return (
     <div id="app" className="bg-green-800 h-max">
       <NavBar />
       {!waitingForAuth && !processingLogin ? (
-        content
+        <Game />
       ) : (
         <div className="flex justify-center items-center h-gamearea">
           <Spinner className="w-20" />
