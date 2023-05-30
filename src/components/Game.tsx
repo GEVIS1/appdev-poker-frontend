5243c36c8b398b2ecc52a359eb989426e0d2a36e
Steffen Geving <gevis1@student.op.ac.nz>
2023-05-30 16:55:12 +1200

    feat(Game.tsx): move lobby to top of page
    instead of centering with flexbox keep the lobby box at the top and let it grow downwards, this makes more sense since the create game button would jump around otherwise

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,12 +1,12 @@
 import { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 
-import Lobby from './Lobby';
+import Lobby from './Lobby/Lobby';
 
 function Game() {
   const { user } = useContext(AuthContext);
   return (
-    <div id="game" className="flex justify-center items-center h-gamearea">
+    <div id="game" className="flex justify-center pt-6 h-gamearea">
       <Lobby user={user} />
     </div>
   );
