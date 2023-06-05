28a6aab8faf3e8f3aa41b2afca3f98d6a0d0060b
Steffen Geving <gevis1@student.op.ac.nz>
2023-06-06 11:50:59 +1200

    feat: wrap Lobby component with the LobbyProvider in Game.tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,12 +2,15 @@ import { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 
 import Lobby from './Lobby/Lobby';
+import LobbyProvider from '../contexts/LobbyContext';
 
 function Game() {
   const { user } = useContext(AuthContext);
   return (
     <div id="game" className="flex justify-center pt-6 h-gamearea">
-      <Lobby user={user} />
+      <LobbyProvider>
+        <Lobby user={user} />
+      </LobbyProvider>
     </div>
   );
 }
