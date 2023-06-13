01d974b1fe21165b5972404fde42edc842ff9d49
Steffen Geving <gevis1@student.op.ac.nz>
2023-06-13 20:10:24 +1200

    feat(Game.tsx): add use of Poker component

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,26 +1,27 @@
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 
 import Lobby from './Lobby';
+import Poker from './Poker';
 import LobbyProvider from '../contexts/LobbyContext';
 import { GameContext } from '../contexts/GameContext';
 
 function Game() {
   const { user } = useContext(AuthContext);
-  const { inGame, currentGame, leaveGame } = useContext(GameContext);
+  const { inGame } = useContext(GameContext);
 
-  if (inGame)
+  if (inGame) {
     return (
-        <div id="game" className="flex justify-center p-6 h-gamearea">
-          <div>You are in a game.</div>
-          <button type="button" onClick={() => leaveGame(currentGame)}> Leave Game </button>
-        </div>
+      <div id="game" className="flex justify-center p-6 h-gamearea">
+        <Poker />
+      </div>
     );
+  }
   return (
     <div id="game" className="flex justify-center p-6 h-gamearea">
-        <LobbyProvider>
-          <Lobby user={user} />
-        </LobbyProvider>
+      <LobbyProvider>
+        <Lobby user={user} />
+      </LobbyProvider>
     </div>
   );
 }
