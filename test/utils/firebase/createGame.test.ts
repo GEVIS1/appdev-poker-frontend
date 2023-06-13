71852b1ec93b82190499d375c5f8a015751a91af
Steffen Geving <gevis1@student.op.ac.nz>
2023-06-13 20:15:43 +1200

    test: add tests for creating games

--- test/utils/firebase/createGame.test.ts
+++ test/utils/firebase/createGame.test.ts
@@ -1,5 +1,11 @@
 import {
-  vi, expect, describe, it,
+  vi,
+  expect,
+  describe,
+  it,
+  beforeEach,
+  afterEach,
+  afterAll,
 } from 'vitest';
 import { readFileSync } from 'fs';
 import { faker } from '@faker-js/faker';
@@ -8,10 +14,18 @@ import {
   assertSucceeds,
   initializeTestEnvironment,
 } from '@firebase/rules-unit-testing';
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
+import { User } from 'firebase/auth';
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import firebase, { firestore } from '../../../src/utils/firebase/firebase';
+/* eslint-enable @typescript-eslint/no-unused-vars */
 
 import firebaseConfig from '../../../firebaseConfig.json';
 import firebaseJson from '../../../firebase.json';
+import createGame from '../../../src/utils/firebase/createGame';
 
 const testEnvironment = await initializeTestEnvironment({
   projectId: firebaseConfig.projectId,
@@ -32,7 +46,11 @@ const aliceFirestore = alice.firestore();
 const unauthed = testEnvironment.unauthenticatedContext();
 const unauthedFirestore = unauthed.firestore();
 
-describe('Create Game unit tests', () => {
+beforeEach(async () => {
+  await testEnvironment.clearFirestore();
+});
+
+describe('Firebase /games permissions', () => {
   it('Should allow a logged in user to create a game', async () => {
     const gamesReference = collection(aliceFirestore, 'games');
     await assertSucceeds(
@@ -45,6 +63,7 @@ describe('Create Game unit tests', () => {
       }),
     );
   });
+
   it('Should not allow an unauthorized user to create a game', async () => {
     const gamesReference = collection(unauthedFirestore, 'games');
     await assertFails(
@@ -56,5 +75,42 @@ describe('Create Game unit tests', () => {
         currentTurn: 0,
       }),
     );
+  });
+});
+
+describe('Create Game unit tests', () => {
+  let mockedAliceUser: User;
+  const mockedPrompt = vi.fn(() => 'Cool Game');
+
+  beforeEach(() => {
+    mockedAliceUser = {
+      displayName: 'Alice',
+      uid: aliceUid,
+    } as User;
+
+    vi.stubGlobal('prompt', mockedPrompt);
+  });
+
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
+  afterAll(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('Should successfully create a game for an authorized user', async () => {
+    const docsBefore = await getDocs(collection(aliceFirestore, 'games'));
+    expect(docsBefore.empty).toBe(true);
+    const mockedSetCurrentGame = vi.fn();
+    const mockedSetInGame = vi.fn();
+
+    await assertSucceeds(
+      createGame(mockedAliceUser, mockedSetCurrentGame, mockedSetInGame),
+    );
+
+    expect(mockedPrompt).toHaveBeenCalled();
+    expect(mockedSetCurrentGame).toHaveBeenCalled();
+    expect(mockedSetInGame).toHaveBeenCalled();
   });
 });
