789a0946a7c7388162054e82af652e898d1f6ca4
Steffen Geving <gevis1@student.op.ac.nz>
2023-06-20 09:48:52 +1200

    feat: add display of current game data in Poker.tsx

--- src/components/Poker.tsx
+++ src/components/Poker.tsx
@@ -1,9 +1,28 @@
 import { useContext } from 'react';
+import { DocumentData } from 'firebase/firestore';
 import { GameContext } from '../contexts/GameContext';
 import LeaveGameButton from '../buttons/LeaveGameButton';
+import { Player } from '../utils/poker/poker';
 
 function Poker() {
-  const { currentGame, leaveGame } = useContext(GameContext);
+  const { currentGame, gameData } = useContext(GameContext);
+
+  const renderGameData = (gameInfo: DocumentData) => (
+    <div>
+      {!gameInfo.open && <h1 className="text-5xl">Game Started</h1>}
+      <br />
+      <p>{`Game Name: ${gameInfo.gameName}`}</p>
+      <p>{`Creator: ${gameInfo.creator.name}`}</p>
+      <p>
+        {`Players: ${gameInfo.players
+          .map((p: Player) => p.name)
+          .join(', ')}`}
+      </p>
+      <p>{`Open: ${gameInfo.open ? 'Yes' : 'No'}`}</p>
+      <p>{`Current Turn: ${gameInfo.currentTurn}`}</p>
+      <p>{`Created At: ${gameInfo.createdAt.toDate()}`}</p>
+    </div>
+  );
 
   if (currentGame) {
     return (
@@ -16,6 +35,7 @@ function Poker() {
           id="game"
           className="flex flex-col gap-2 justify-center items-center p-6 h-gamearea"
         >
+          {gameData && renderGameData(gameData)}
           <LeaveGameButton />
         </div>
       </div>
