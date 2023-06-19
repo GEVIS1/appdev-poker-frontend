7d3ff535716a87cdaae557c57659f1068f712e12
Steffen Geving <gevis1@student.op.ac.nz>
2023-06-20 10:22:49 +1200

    feat: add hands field to PokerGame class
    this uses the new Hand and Card interfaces along with the Suit and Rank enums

--- src/utils/poker/poker.ts
+++ src/utils/poker/poker.ts
@@ -8,12 +8,45 @@ export interface Player {
   name: string;
   uid: string;
 }
+
+export enum Suit {
+  Spade = 'SPADE',
+  Club = 'CLUB',
+  Heart = 'HEART',
+  Diamond = 'DIAMOND',
+}
+export enum Rank {
+  TWO = 2,
+  THREE = 3,
+  FOUR = 4,
+  FIVE = 5,
+  SIX = 6,
+  SEVEN = 7,
+  EIGHT = 8,
+  NINE = 9,
+  TEN = 10,
+  JACK = 11,
+  QUEEN = 12,
+  KING = 13,
+  ACE = 14,
+}
+
+export interface Card {
+  suit: Suit;
+  rank: Rank;
+}
+export interface Hand {
+  cards: [Card, Card, Card, Card, Card];
+}
+
 class PokerGame {
   gameName: string;
 
   creator: Player;
 
   players: Array<Player>;
+
+  hands: Array<Hand>;
 
   open: boolean;
 
@@ -26,6 +59,7 @@ class PokerGame {
     creator: Player,
     players: Array<Player>,
     gameId: string,
+    hands: Array<Hand> = [],
     open = true,
     currentTurn = 0,
   ) {
@@ -33,6 +67,7 @@ class PokerGame {
     this.creator = creator;
     this.players = players;
     this.gameId = gameId;
+    this.hands = hands;
     this.open = open;
     this.currentTurn = currentTurn;
   }
@@ -44,6 +79,7 @@ const pokerGameConverter = {
       gameName: pokerGame.gameName,
       creator: pokerGame.creator,
       players: pokerGame.players,
+      hands: pokerGame.hands,
       open: pokerGame.open,
       currentTurn: pokerGame.currentTurn,
     };
@@ -58,6 +94,7 @@ const pokerGameConverter = {
       data.creator,
       data.players,
       snapshot.id,
+      data.hands,
       data.open,
       data.currentTurn,
     );
