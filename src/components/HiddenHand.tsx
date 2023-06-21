3d84b636129a034adf94a2facffaf28ce7c254d1
Steffen Geving <gevis1@student.op.ac.nz>
2023-06-22 11:59:45 +1200

    fix: change key prop to keyProp due to name clash

--- src/components/HiddenHand.tsx
+++ src/components/HiddenHand.tsx
@@ -1,15 +1,15 @@
 import CardComponent from './CardComponent';
 
 interface HiddenHandProps {
-  key: string;
+  keyProp: string;
 }
 
-function HiddenHand({ key }: HiddenHandProps) {
+function HiddenHand({ keyProp }: HiddenHandProps) {
   return (
     <div
       id="otherHand"
       className="flex flex-row gap-5 border-2 rounded-lg px-5 pb-4 pt-1 border-green-900"
-      key={key}
+      key={keyProp}
     >
       {[0, 1, 2, 3, 4].map((card) => (
         <CardComponent className="text-8xl" key={card} faceDown />
