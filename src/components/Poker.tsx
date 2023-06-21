615c61d5d5e3fa4c34b3aab1f43f43bbdbf5bb2c
Steffen Geving <gevis1@student.op.ac.nz>
2023-06-21 20:29:25 +1200

    feat(Poker): add rendering of my hand and other players hands
    using default state for now
    
    but this can easily be plugged in with the game's state loaded from firestore

--- src/components/Poker.tsx
+++ src/components/Poker.tsx
@@ -1,29 +1,36 @@
-import { useContext } from 'react';
-import { DocumentData } from 'firebase/firestore';
+import { useContext, useEffect, useState } from 'react';
 import { GameContext } from '../contexts/GameContext';
+import StartGameButton from '../buttons/StartGameButton';
 import LeaveGameButton from '../buttons/LeaveGameButton';
 import { AuthContext } from '../contexts/AuthContext';
-import { Player } from '../utils/firebase/poker';
-import startGame from '../utils/firebase/startGame';
+import PokerClass, {
+  Card, Hand, Rank, Suit,
+} from '../utils/poker/game';
+import CardComponent from './CardComponent';
+
+const poker = new PokerClass();
+
+const defaultHand: Hand = poker.dealAHand();
+const defaultOtherHands: Hand[] = [
+  poker.dealAHand(),
+  poker.dealAHand(),
+  poker.dealAHand(),
+];
 
 function Poker() {
   const { currentGame, gameData } = useContext(GameContext);
   const { user } = useContext(AuthContext);
-
-  const renderGameData = (gameInfo: DocumentData) => (
-    <div>
-      {!gameInfo.open && <h1 className="text-5xl">Game Started</h1>}
-      <br />
-      <p>{`Game Name: ${gameInfo.gameName}`}</p>
-      <p>{`Creator: ${gameInfo.creator.name}`}</p>
-      <p>
-        {`Players: ${gameInfo.players.map((p: Player) => p.name).join(', ')}`}
-      </p>
-      <p>{`Open: ${gameInfo.open ? 'Yes' : 'No'}`}</p>
-      <p>{`Current Turn: ${gameInfo.currentTurn}`}</p>
-      <p>{`Created At: ${gameInfo.createdAt.toDate()}`}</p>
-    </div>
+  const [isOwner, setIsOwner] = useState(
+    gameData && user && gameData.creator.uid === user.uid,
   );
+  const [currentHand, setCurrentHand] = useState<Hand>(defaultHand);
+  const [otherHands, setOtherHands] = useState<Hand[]>(defaultOtherHands);
+
+  useEffect(() => {
+    if (gameData && user) {
+      setIsOwner(gameData.creator.uid === user.uid);
+    }
+  }, [gameData, user]);
 
   if (currentGame) {
     return (
@@ -34,23 +41,48 @@ function Poker() {
       >
         <div
           id="game"
-          className="flex flex-col gap-2 justify-center items-center p-6 h-gamearea"
+          className="flex flex-col gap-7 justify-center items-center p-6 h-gamearea"
         >
-          {gameData
-          && user
-          && gameData.open
-          && gameData.creator.uid === user.uid ? (
-            <button
-              type="button"
-              onClick={() => {
-                startGame(user, currentGame);
-              }}
-            >
-              Start Game
-            </button>
-            ) : null}
-          {gameData && renderGameData(gameData)}
-          <LeaveGameButton />
+          {otherHands.map((hand, i) => (
+            <>
+              <div>
+                Player
+                {i + 2}
+              </div>
+              <div
+                id="otherHand"
+                className="flex flex-row gap-5"
+                key={hand.cards
+                  .map((card: Card) => card.suit + card.rank)
+                  .join('')}
+              >
+                {hand.cards.map((card) => (
+                  <CardComponent
+                    className="text-8xl"
+                    key={card.suit + card.rank}
+                    // card={card}
+                    faceDown
+                  />
+                ))}
+              </div>
+            </>
+          ))}
+          <div className="flex flex-col justify-center items-center mt-auto mb-auto">
+            My Hand
+            <div id="myHand" className="flex flex-row gap-5">
+              {currentHand.cards.map((card) => (
+                <CardComponent
+                  className="text-9xl"
+                  key={card.suit + card.rank}
+                  card={card}
+                />
+              ))}
+            </div>
+          </div>
+          <div id="gameButtons" className="flex flex-row gap-2">
+            {user && isOwner ? <StartGameButton /> : null}
+            <LeaveGameButton />
+          </div>
         </div>
       </div>
     );
