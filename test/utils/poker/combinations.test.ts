88173ab6be8b70fc0c606d98405e6d19a5941e2f
Steffen Geving <gevis1@student.op.ac.nz>
2023-06-23 11:30:23 +1200

    test: add test for two pairs

--- test/utils/poker/combinations.test.ts
+++ test/utils/poker/combinations.test.ts
@@ -434,25 +434,89 @@ describe('combinations tests', () => {
     }
   });
 
-  // it('Two Pairs', () => {
-  //   const combination = combinations[7];
-  //   const hands: Array<[NonNullableHand, boolean]> = [
-  //     [{
-  //     cards: [
-  //       { suit: Suit.Spade, rank: Rank.ACE },
-  //       { suit: Suit.Spade, rank: Rank.ACE },
-  //       { suit: Suit.Spade, rank: Rank.ACE },
-  //       { suit: Suit.Spade, rank: Rank.ACE },
-  //       { suit: Suit.Spade, rank: Rank.ACE },
-  //     ]
-  //   }, true],
-  //   ]
+  it('Two Pairs', () => {
+    const combination = combinations[7];
+    const hands: Array<[NonNullableHand, boolean]> = [
+      [
+        {
+          cards: [
+            { suit: Suit.Spade, rank: Rank.TWO },
+            { suit: Suit.Spade, rank: Rank.ACE },
+            { suit: Suit.Spade, rank: Rank.TWO },
+            { suit: Suit.Spade, rank: Rank.FOUR },
+            { suit: Suit.Spade, rank: Rank.ACE },
+          ],
+        },
+        true,
+      ],
+      [
+        {
+          cards: [
+            { suit: Suit.Spade, rank: Rank.TWO },
+            { suit: Suit.Spade, rank: Rank.THREE },
+            { suit: Suit.Spade, rank: Rank.TWO },
+            { suit: Suit.Spade, rank: Rank.ACE },
+            { suit: Suit.Spade, rank: Rank.THREE },
+          ],
+        },
+        true,
+      ],
+      [
+        {
+          cards: [
+            { suit: Suit.Spade, rank: Rank.FIVE },
+            { suit: Suit.Spade, rank: Rank.ACE },
+            { suit: Suit.Spade, rank: Rank.QUEEN },
+            { suit: Suit.Spade, rank: Rank.FIVE },
+            { suit: Suit.Spade, rank: Rank.ACE },
+          ],
+        },
+        true,
+      ],
+      [
+        {
+          cards: [
+            { suit: Suit.Spade, rank: Rank.ACE },
+            { suit: Suit.Spade, rank: Rank.TWO },
+            { suit: Suit.Spade, rank: Rank.THREE },
+            { suit: Suit.Spade, rank: Rank.FOUR },
+            { suit: Suit.Spade, rank: Rank.FIVE },
+          ],
+        },
+        false,
+      ],
+      [
+        {
+          cards: [
+            { suit: Suit.Spade, rank: Rank.ACE },
+            { suit: Suit.Spade, rank: Rank.KING },
+            { suit: Suit.Spade, rank: Rank.QUEEN },
+            { suit: Suit.Spade, rank: Rank.JACK },
+            { suit: Suit.Spade, rank: Rank.TEN },
+          ],
+        },
+        false,
+      ],
+      [
+        {
+          cards: [
+            { suit: Suit.Spade, rank: Rank.TWO },
+            { suit: Suit.Spade, rank: Rank.FOUR },
+            { suit: Suit.Spade, rank: Rank.SEVEN },
+            { suit: Suit.Spade, rank: Rank.QUEEN },
+            { suit: Suit.Spade, rank: Rank.THREE },
+          ],
+        },
+        false,
+      ],
+    ];
 
-  //   for (let i = 0; i < hands.length; i += 1) {
-  //     const result = combination.evaluate(hands[i][0]);
-  //     expect(result).toBe(hands[i][1]);
-  //   }
-  // });
+    for (let i = 0; i < hands.length; i += 1) {
+      const result = combination.evaluate(hands[i][0]);
+      expect(result).toBe(hands[i][1]);
+    }
+  });
+
   // it('One Pair', () => {
   //   const combination = combinations[8];
   //   const hands: Array<[NonNullableHand, boolean]> = [
