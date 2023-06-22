a9b49ecbd91460cd46cce129747631bf66928093
Steffen Geving <gevis1@student.op.ac.nz>
2023-06-22 18:00:01 +1200

    fix: remove unneccesary key prop from HiddenHand

--- src/components/HiddenHand.tsx
+++ src/components/HiddenHand.tsx
@@ -1,18 +1,13 @@
 import CardComponent from './CardComponent';
 
-interface HiddenHandProps {
-  keyProp: string;
-}
-
-function HiddenHand({ keyProp }: HiddenHandProps) {
+function HiddenHand() {
   return (
     <div
       id="otherHand"
       className="flex flex-row gap-5 border-2 rounded-lg px-5 pb-4 pt-1 border-green-900"
-      key={keyProp}
     >
       {[0, 1, 2, 3, 4].map((card) => (
-        <CardComponent className="text-8xl" key={keyProp + card} faceDown />
+        <CardComponent className="text-8xl" key={card} faceDown />
       ))}
     </div>
   );
