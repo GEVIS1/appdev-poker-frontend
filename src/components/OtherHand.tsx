442daf0f6c6883426a22b5835115e3f66269c650
Steffen Geving <gevis1@student.op.ac.nz>
2023-06-22 17:14:19 +1200

    feat(OtherHand): add display of winner and combination

--- src/components/OtherHand.tsx
+++ src/components/OtherHand.tsx
@@ -1,3 +1,4 @@
+import { Result } from '../utils/firebase/poker';
 import { Hand } from '../utils/poker/game';
 import CardComponent from './CardComponent';
 
@@ -5,11 +6,18 @@ interface OtherHandProps {
   hand: Hand;
   currentTurn: boolean;
   playerName: string;
-  score: number | undefined;
+  playerIndex: number;
+  winnerIndex: number;
+  result: Result;
 }
 
 function OtherHand({
-  hand, currentTurn, playerName, score,
+  hand,
+  currentTurn,
+  playerName,
+  playerIndex,
+  winnerIndex,
+  result,
 }: OtherHandProps) {
   if (hand.cards === null) {
     return <div>No cards in hand...</div>;
@@ -20,7 +28,7 @@ function OtherHand({
       className={`flex flex-col border-2 rounded-lg px-5 pb-4 pt-3 ${
         // If it's the player's turn, highlight their hand
         currentTurn ? 'border-yellow-600' : 'border-green-900'
-      }`}
+      } ${winnerIndex === playerIndex ? 'border-blue-500' : ''}`}
     >
       <div className="flex flex-row gap-5">
         {hand.cards.map((card, i) => (
@@ -33,7 +41,9 @@ function OtherHand({
       </div>
       <div className="flex flex-row justify-between gap-5 pt-2">
         <div>{`Name: ${playerName}`}</div>
-        {score ? <div>{`Score: ${score}`}</div> : null}
+        {result ? (
+          <div>{`Score: ${result.combination} (${result.score})`}</div>
+        ) : null}
       </div>
     </div>
   );
