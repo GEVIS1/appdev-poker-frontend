b25f888c57cd9a682850daef8afd99adc0e742b7
Steffen Geving <gevis1@student.op.ac.nz>
2023-06-22 17:18:27 +1200

    feat(endturn.ts): implement game logic
    endTurn now iterates the currentTurn to be the index of the next player in the players array. if the current index is the last player in the array set the turn to -1 and calculate scores for the game

--- src/utils/firebase/endTurn.ts
+++ src/utils/firebase/endTurn.ts
@@ -1,9 +1,52 @@
-import { User } from 'firebase/auth';
+import { setDoc } from 'firebase/firestore';
+import Poker, { Hand } from '../poker/game';
+import getGameData from './getGameData';
 
-async function endTurn(user: User, currentGame: string): Promise<void> {
-  throw new Error(
-    `Function not implemented. ${JSON.stringify(user)} ${currentGame}`,
-  );
+// user, yourIndex, currentGame, currentHand
+async function endTurn(
+  playerIndex: number,
+  gameId: string,
+  currentHand: Hand,
+): Promise<void> {
+  try {
+    const [gameDocument, gameDocRef] = await getGameData(gameId);
+
+    if (gameDocument.currentTurn !== playerIndex) {
+      throw new Error('Only the player whose turn it is can end their turn.');
+    }
+
+    if (gameDocument.players.length < 2) {
+      throw new Error('Not enough players to start the game.');
+    }
+
+    // TODO: write turn data to firestore here
+    gameDocument.hands[playerIndex] = currentHand;
+
+    /*
+     * If the player is the last player in the list calculate
+     * the scores and end the game by setting the turn to -1
+     */
+    if (playerIndex >= gameDocument.players.length - 1) {
+      gameDocument.currentTurn = -1;
+
+      // Iterate the player hands and load the results of their hands into the results field
+      for (let i = 0; i < gameDocument.players.length; i += 1) {
+        const [combination, score] = Poker.calculateScore(
+          gameDocument.hands[i],
+        );
+        gameDocument.results[i] = { score, combination };
+      }
+    } else {
+      gameDocument.currentTurn += 1;
+    }
+
+    await setDoc(gameDocRef, gameDocument, { merge: true });
+  } catch (e) {
+    // eslint-disable-next-line no-alert
+    alert(e);
+    // eslint-disable-next-line no-console
+    console.error(e);
+  }
 }
 
 export default endTurn;
