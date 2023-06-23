a2b41bc980b06aaddbc5c20d1568660aa30c1f34
Steffen Geving <gevis1@student.op.ac.nz>
2023-06-23 13:53:59 +1200

    build: configure vite plugin PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,45 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 /* eslint-enable import/no-extraneous-dependencies */
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      manifest: {
+        name: 'Appdev Poker gevis1',
+        short_name: 'Poker',
+        description: 'A poker game',
+        theme_color: '#00CC00',
+        icons: [
+          {
+            src: 'poker-icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'poker-icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'poker-mask-icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   test: {
     environment: 'jsdom',
     setupFiles: './test/setup.ts',
