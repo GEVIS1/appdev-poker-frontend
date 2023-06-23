a225ab3b32cecfbe95de68811c559ae4e91a1819
Steffen Geving <gevis1@student.op.ac.nz>
2023-06-23 12:27:08 +1200

    test(game.ts): add tests
    test checking if an error is thrown if attempting to deal a card when deck is empty. test that all combinations can be met

--- test/utils/poker/game.test.ts
+++ test/utils/poker/game.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import Poker from '../../../src/utils/poker/game';
+import Poker, { Hand, Rank, Suit } from '../../../src/utils/poker/game';
 
 describe('Poker game tests', () => {
   it('Should properly shuffle the deck', () => {
@@ -40,5 +40,88 @@ describe('Poker game tests', () => {
 
     expect(game.cardsLeft).toEqual(game.deck.length);
     expect(game.cardsLeft).toEqual(oldDeckLength - game.discard.length);
+  });
+
+  it('Should throw an error when dealing a card from an empty deck', () => {
+    const game = new Poker();
+
+    const iterations = game.deck.length;
+
+    for (let i = 0; i < iterations; i += 1) {
+      game.dealACard();
+    }
+
+    expect(() => game.dealACard()).toThrow('Deck is empty.');
+  });
+
+  it('Should correctly calculate the scores for all the possible combinations', () => {
+    const game = new Poker();
+
+    const iterations = 2e5;
+
+    const scores = {};
+
+    for (let i = 0; i < iterations; i += 1) {
+      game.shuffleDeck();
+
+      let hand: Hand;
+
+      try {
+        hand = game.dealAHand();
+      } catch (e) {
+        game.constructDeck();
+        game.shuffleDeck();
+        hand = game.dealAHand();
+      }
+
+      const [combination] = Poker.calculateScore(hand);
+
+      scores[combination] = (scores[combination] || 0) + 1;
+    }
+
+    /*
+     * We only do 200000 iterations here, so we can't expect a royal flush every time.
+     * So we just run a royal flush through at the end to make sure it's accounted for.
+     */
+
+    const royalFlushHand: Hand = {
+      cards: [
+        {
+          suit: Suit.Spade,
+          rank: Rank.ACE,
+        },
+        {
+          suit: Suit.Spade,
+          rank: Rank.KING,
+        },
+        {
+          suit: Suit.Spade,
+          rank: Rank.QUEEN,
+        },
+        {
+          suit: Suit.Spade,
+          rank: Rank.JACK,
+        },
+        {
+          suit: Suit.Spade,
+          rank: Rank.TEN,
+        },
+      ],
+    };
+
+    const [combination] = Poker.calculateScore(royalFlushHand);
+
+    scores[combination] = (scores[combination] || 0) + 1;
+
+    expect(scores['Royal Flush']).toBeGreaterThan(0);
+    expect(scores['Straight Flush']).toBeGreaterThan(0);
+    expect(scores['Four of a Kind']).toBeGreaterThan(0);
+    expect(scores['Full House']).toBeGreaterThan(0);
+    expect(scores['Flush']).toBeGreaterThan(0);
+    expect(scores['Straight']).toBeGreaterThan(0);
+    expect(scores['Three of a Kind']).toBeGreaterThan(0);
+    expect(scores['Two Pairs']).toBeGreaterThan(0);
+    expect(scores['One Pair']).toBeGreaterThan(0);
+    expect(scores['High Card']).toBeGreaterThan(0);
   });
 });
